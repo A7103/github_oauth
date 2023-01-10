2c834442ebb5336fd138367eb27abdbbe6cbf07f
Pan <117614950+A7103@users.noreply.github.com>
2023-01-10 15:04:17 +0800

    Add config.go

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/a7103/github_oauth/service"
+	"github.com/a7103/github_oauth/views"
+	"github.com/jinzhu/configor"
+)
+
+var _config *service.GithubOauthConfig
+
+func MustGetGithubOauthConfig() service.GithubOauthConfig {
+	if _config != nil {
+		return *_config
+	}
+
+	var cfg service.GithubOauthConfig
+	err := configor.New(&configor.Config{ENVPrefix: "GITHUB_OAUTH", Debug: true}).Load(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	_config = &cfg
+
+	return *_config
+}
+
+func MustGetOauthServ() *service.OauthServ {
+	return service.MustGetOauthServ(
+		views.MustGetTemplate(),
+		MustGetGithubOauthConfig(),
+	)
+}
